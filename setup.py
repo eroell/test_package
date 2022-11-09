3c3b77acd39afb5ab2e3cd8559703b947aba8f89
Eljas <eljas.roellin@gmail.com>
2022-11-09 09:57:54 +0100

    v0.0.2

--- setup.py
+++ setup.py
@@ -4,15 +4,15 @@ with open("README.md", "r") as fh:
     description = fh.read()
   
 setuptools.setup(
-    name="test-package",
-    version="0.0.1",
+    name="test-package-id67",
+    version="0.0.2",
     author="er",
     author_email="david.hermann@gmail.com",
-    packages=["test_package"],
+    packages=["test_package_id67"],
     description="A sample test package",
     long_description=description,
     long_description_content_type="text/markdown",
-    url="https://github.com/gituser/test-tackage",
+    url="https://github.com/eroell/test_package",
     license='MIT',
     python_requires='>=3.8',
     install_requires=[]
